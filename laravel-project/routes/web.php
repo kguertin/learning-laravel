1ef6026fab0290cb13e8099fe4ca9b3a74ddd7ca
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-24 12:56:12 -0400

    use routing and generate urls

--- laravel-project/routes/web.php
+++ laravel-project/routes/web.php
@@ -11,34 +11,34 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 // Route::get('/', function () {
 //     return view('home');
 // });
 
-Route::view('/', 'home');
+Route::view('/', 'home')->name('home');
 
 // Route::get('/contact', function(){
 //     return view('contact');
 // });
 
-Route::view('/contact', 'contact');
+Route::view('/contact1', 'contact')->name('contact');
 
 Route::get('/blog-post/{id}/{welcome?}', function ($id, $welcome = 1) {
 
     $pages = [
         1 => [
-            'title' => "page 1"
+            'title' => "page 1",
         ],
         2 => [
-            "title" => "page 2"
-        ]
-        ];
-        $welcomes = [
-            1 => "<b>Hello from</b> ",
-            2 => "<b>Welcome to</b> "
-        ];
+            "title" => "page 2",
+        ],
+    ];
+    $welcomes = [
+        1 => "<b>Hello from</b> ",
+        2 => "<b>Welcome to</b> ",
+    ];
 
-        return view('blog-post', ['data' => $pages[$id], 'welcome' => $welcomes[$welcome]]);
-});
+    return view('blog-post', ['data' => $pages[$id], 'welcome' => $welcomes[$welcome]]);
+})->name('blog-post');
