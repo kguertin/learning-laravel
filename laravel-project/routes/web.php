dc50d11d04a79833e074bd158c7c40156c9427ee
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-24 21:36:28 -0400

    add resource controller for posts

--- laravel-project/routes/web.php
+++ laravel-project/routes/web.php
@@ -14,7 +14,6 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/', 'HomeController@home')->name('home');
-
 Route::get('/contact', 'HomeController@contact')->name('contact');
+Route::resource('/posts', 'PostController')->only('index', 'show');
 
-Route::get('/blog-post/{id}/{welcome?}', 'HomeController@blogPost')->name('blog-post');
