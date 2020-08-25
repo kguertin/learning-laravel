0114c869eb0cb4912cbd51f2707a73c95c5c495e
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-24 21:47:04 -0400

    render blog post information using foreach and views

--- laravel-project/app/Http/Controllers/PostController.php
+++ laravel-project/app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        dd(BlogPost::all());
+        return view('posts.index', ['posts' => BlogPost::all()]);
     }
 
     /**
